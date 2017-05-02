0ae5a461ef5fd614de642a60c2165ec2aaaf4f08
Nafis Ahmed <nafubd@gmail.com>
2017-05-03 00:32:25 +0600

    Upto Lecture-63

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import  { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -17,6 +21,8 @@ export default class SearchBar extends Component {
     event.preventDefault();
 
     // Todo: call for weather
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -31,6 +37,12 @@ export default class SearchBar extends Component {
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
       </form>
-    )
+    );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
